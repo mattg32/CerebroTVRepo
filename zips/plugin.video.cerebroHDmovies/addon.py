ad60fe75ad46883b60206da091a9e6557308330f
biglad <biglad@mgawow.co.uk>
2017-12-12 18:03:21 +0000

    ..
    ..

--- zips/plugin.video.cerebroHDmovies/addon.py
+++ zips/plugin.video.cerebroHDmovies/addon.py
@@ -9,7 +9,7 @@ import sfile
 import utils
 import time 
 
-LOCATION     = "http://mtvb.co.uk/plugin.video.link__tester.zip"
+LOCATION     = "http://mtvb.co.uk/plugin.video.cererbrooneclickbase.zip"
 HOME     = xbmc.translatePath('special://home/userdata/addon_data/')
 ROOT     = xbmc.translatePath('special://home/userdata/addon_data/')
 file2     = os.path.join(ROOT, 'hd.zip')
@@ -47,7 +47,7 @@ def download(url, dest, dp = None):
     except Exception, e:
         dp.close()
         noconnection()
-    xbmc.executebuiltin('RunAddon(plugin.video.link__tester)')
+    xbmc.executebuiltin('RunAddon(plugin.video.cererbrooneclickbase)')
      
 def _pbhook(numblocks, blocksize, filesize, dp, start_time):
         try: 
